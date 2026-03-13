08e531b6540b2495932fd249762cd6e7be216a65
nisulaRap <nisula.rap@gmail.com>
2026-03-13 11:06:20 +0530

    updated payment-froup instead of billing-service

--- payment-service/src/main/java/com/example/payment_service/consumer/PaymentConsumer.java
+++ payment-service/src/main/java/com/example/payment_service/consumer/PaymentConsumer.java
@@ -5,7 +5,7 @@ import org.springframework.stereotype.Service;
 
 @Service
 public class PaymentConsumer {
-    @KafkaListener(topics = "order-topic", groupId = "billing-group")
+    @KafkaListener(topics = "order-topic", groupId = "payment-group")
     public void consume(String message) {
 
         System.out.println("Invoice Generated for Order: " + message);
