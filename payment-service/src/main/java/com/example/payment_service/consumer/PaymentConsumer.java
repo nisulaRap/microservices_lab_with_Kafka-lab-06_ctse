7a2cdcf6b2c022573e65ba2cc275a195fa257ba3
nisulaRap <nisula.rap@gmail.com>
2026-03-13 09:44:29 +0530

    added PaymentConsumer to listen for order updates and generate invoices

--- payment-service/src/main/java/com/example/payment_service/consumer/PaymentConsumer.java
+++ payment-service/src/main/java/com/example/payment_service/consumer/PaymentConsumer.java
@@ -1,5 +1,13 @@
 package com.example.payment_service.consumer;
 
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.stereotype.Service;
+
+@Service
 public class PaymentConsumer {
-    
+    @KafkaListener(topics = "order-topic", groupId = "billing-group")
+    public void consume(String message) {
+
+        System.out.println("Invoice Generated for Order: " + message);
+    }
 }
