7e8acdfdebc1f4f605c7e496f04fef2777bce085
nisulaRap <nisula.rap@gmail.com>
2026-03-13 10:57:51 +0530

    added InventoryConsumer to handle order updates from Kafka

---
+++ inventory-service/src/main/java/com/example/inventory_service/consumer/InventoryConsumer.java
@@ -0,0 +1,13 @@
+package com.example.inventory_service.consumer;
+
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.stereotype.Service;
+
+@Service
+public class InventoryConsumer {
+    @KafkaListener(topics = "order-topic", groupId = "inventory-group")
+    public void consume(String message) {
+
+        System.out.println("Inventory Updated for Order: " + message);
+    }
+}
